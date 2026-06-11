8e7b8cbc77b414f54236b054b0d6d30367d06c6a
Ishan Timalsina <itsmeishan14@gmail.com>
2026-06-11 10:50:11 +0545

    Third commit

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 2 ) we are using node js not the browser
+
+console.log(2 + 2);
+console.log(" Ishan ");// code readability should be high 
+
+let name = "ishan"// string data types
+let age = 20 // integer number 2^53 something after that big integer we use
+let isLoggedIn= true // boolen algebra
+// these are primitive data types 
+//we can use string in single quote or double quote 
+// boolean => true/false 
+//bigint for big vlue of integer 
+// null => standalone value it is the reprresentation of the empty value we mention zero for example
+// undefined => when the value is not assigned to the variable 
+//symbol => unique 
+
+
+
+
+// non-primitive
+console.log(typeof"ishan");//type of null is always object in javascript where as type of undefined is always undefined
