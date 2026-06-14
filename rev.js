a0b8f54aa4ac057d3a2fcaa46175e28ea96ab64b
Ishan Timalsina <itsmeishan14@gmail.com>
2026-06-14 21:10:25 +0545

    revision on the topic

---
+++ rev.js
@@ -0,0 +1,27 @@
+//javascript is the programming language which helps the connection or talks with the web pages in the website its the logic its used alongsid the html and css.
+console.log("Hello ");
+// javascript important topic : varibale: varibale is the contianer where we store the data is the place memenory where we store the data nothing more than that 
+//for declaring the varibale we have 3 types which are let var const in which const is fixed one we declare the variable with the const we cannot chnage it where var is the global scope varibale which can be changed 
+// for exaple i will show you the difference between the let and var 
+
+var name="Ishan "//its global scope varibale once its made it can be changed 
+var name="hari"
+console.log(name);
+let nam="ishan"
+
+console.log(nam);
+// this is the difference between the var and let ways to declare the varaibale in which we can see that after using the lete we cannot use the another let to declare it and its the block scope varible
+// in the earliar days when many people where working on the project and one has decalared the keywoerk with the var and kept the name as ishan and another person aslo tries and same keyworkd and kept the name as the different person and change into the different than the errors where there.So to remove such errosr the js made the new upadate and started with let keyworkds.
+nam='is'
+console.log(nam);
+
+// these were the difference between the var and name when you pratice as much as you can you will get the more concept as you keep going 
+// in javascript the data types is categorized into two types which are primitive data types and non-permitive data types in the js 
+// in primitive data types we have the number stirng boolean undefined null bigint and sysmbol and in the non-permitive we have array and function 
+// lets go with primitive data types
+let num=123;
+let na="string"//these double inverted comma or the single comma detenmines whether it is the string or not 
+ let is_true=false;// boolean numbers 
+// we have big int for big number of interger sometinng greather than 2^53 above I guess 
+//symbobsl undefined no value
+
