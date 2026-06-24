080e4dfa00e24e64671827c31c559c96c7d0092e
Ishan Timalsina <itsmeishan14@gmail.com>
2026-06-24 16:09:18 +0545

    backtick learned

---
+++ codestructure.js
@@ -0,0 +1,9 @@
+// backticks 
+// backticks are extended functionality quotes they allow us to embed varibales and expression into a string by wrapping them in ${..}
+let name = 'John';
+alert (`Hello,${name}!`)
+
+
+//confirm 
+let isBoss = confirm("Are you the boss")
+alert(isBoss);
